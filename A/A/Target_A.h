e210bc4adebb197f0943d8cf1120bc1871d756a0
Nanmenhaishao <708849329@qq.com>
2018-01-25 20:16:34 +0800

    1

---
+++ A/A/Target_A.h
@@ -0,0 +1,15 @@
+//
+//  Target_A.h
+//  A
+//
+//  Created by 张常伟 on 2018/1/25.
+//  Copyright © 2018年 张常伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Target_A : NSObject
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params;
+
+@end
---
+++ A/A/Target_A.m
@@ -0,0 +1,20 @@
+//
+//  Target_A.m
+//  A
+//
+//  Created by 张常伟 on 2018/1/25.
+//  Copyright © 2018年 张常伟. All rights reserved.
+//
+
+#import "Target_A.h"
+#import "AViewController.h"
+
+@implementation Target_A
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params
+{
+    AViewController *viewController = [[AViewController alloc] init];
+    return viewController;
+}
+
+@end
